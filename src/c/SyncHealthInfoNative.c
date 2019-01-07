25a63cac729cfc17a10f463484c836e1b900588f
amirfahmideh <amir.fahmideh@hotmail.com>
2019-01-07 12:13:24 +0330

    Add default time to system

--- src/c/SyncHealthInfoNative.c
+++ src/c/SyncHealthInfoNative.c
@@ -30,8 +30,9 @@ static void prv_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  s_text_layer = text_layer_create(GRect(0, 72, bounds.size.w, 20));
+  s_text_layer = text_layer_create(GRect(0, 72, bounds.size.w, 40));
   text_layer_set_text(s_text_layer, "Press a button");
+  text_layer_set_font(s_text_layer, fonts_get_system_font(FONT_KEY_LECO_26_BOLD_NUMBERS_AM_PM));
   text_layer_set_text_alignment(s_text_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(s_text_layer));
 }

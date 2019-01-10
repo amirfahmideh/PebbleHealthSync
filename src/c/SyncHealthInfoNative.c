6c6aa1c079099e77c8abb6a8fc5b73f2fb1c2d1b
amirfahmideh <amir.fahmideh@hotmail.com>
2019-01-10 08:06:24 +0330

    fill all text box in page load

--- src/c/SyncHealthInfoNative.c
+++ src/c/SyncHealthInfoNative.c
@@ -1,13 +1,11 @@
 #include <pebble.h>
-#include "windows/dialog_message_window.h"
-#include "goalwindow.h"
-#include "local_storage.h"
 #include "persiancalendar.h"
-// #include "worker/step_count_worker.c"
+#include "windows/dialog_message_window.h"
+#include "local_storage.h"
 
+// #include "worker/step_count_worker.c"
 static Window *s_window;
 static NumberWindow *s_window_goals;
-
 
 static TextLayer *s_text_layer;
 static TextLayer *s_text_persianDate;
@@ -15,12 +13,32 @@ static TextLayer *s_text_goalNumber;
 static TextLayer *s_text_todayStepNumber;
 static TextLayer *s_text_ofLabel;
 
+static int LastStepNumberFromBackground = 0;
+
+//Set today date and time!
+static void setTodayTimeAndDate(TextLayer * text_time, TextLayer * text_date){
+  time_t temp = time(NULL);
+  struct tm *tick_time = localtime(&temp);
+  static char s_buffer[8];
+  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
+                                          "%H:%M" : "%I:%M", tick_time);
+  text_layer_set_text(text_time, s_buffer);
+  //update datetime
+  int y, m, d;
+  gregorian_to_jalali(&y, &m, &d,1900+tick_time->tm_year,1+tick_time->tm_mon,tick_time->tm_mday);
+  static char str[20];
+  snprintf(str,sizeof(str), "%d/%d/%d",y,m,d);
+  text_layer_set_text(text_date, str);
+}
+
 static void worker_message_handler(uint16_t type, AppWorkerMessage *data) {
+
+  LastStepNumberFromBackground = data->data2;
   static char s_buffer[32];
-  snprintf(s_buffer, sizeof(s_buffer), "%d", data->data2);
+  snprintf(s_buffer, sizeof(s_buffer), "%d", LastStepNumberFromBackground);
   text_layer_set_text(s_text_todayStepNumber, s_buffer);
   // Do something with the data
-  APP_LOG(APP_LOG_LEVEL_INFO, "got data %i %i %i", data->data0, data->data1, data->data2);
+  // APP_LOG(APP_LOG_LEVEL_INFO, "got data %i %i %i", data->data0, data->data1, data->data2);
 }
 
 static void callOnGoalChanges(int newgoal){
@@ -79,6 +97,7 @@ static void prv_window_load(Window *window) {
   s_text_layer = text_layer_create(GRect(0, 0, bounds.size.w, 60));
   s_text_persianDate = text_layer_create(GRect(0, 60, bounds.size.w, 26));
 
+
   s_text_todayStepNumber = text_layer_create(GRect(0, 102, bounds.size.w, 28));
   s_text_ofLabel = text_layer_create(GRect(0, 130, bounds.size.w, 18));
   s_text_goalNumber = text_layer_create(GRect(0, 148, bounds.size.w, 18));
@@ -110,6 +129,13 @@ static void prv_window_load(Window *window) {
   text_layer_set_text_alignment(s_text_goalNumber, GTextAlignmentCenter);
     // text_layer_set_background_color(s_text_goalNumber,GColorBlack);
 
+  //Set NOW date and time!
+  setTodayTimeAndDate(s_text_layer,s_text_persianDate);
+  //Set default for today step numberWindow
+  static char s_buffer[32];
+  snprintf(s_buffer, sizeof(s_buffer), "%d", LastStepNumberFromBackground);
+  text_layer_set_text(s_text_todayStepNumber, s_buffer);
+
 
   layer_add_child(window_layer, text_layer_get_layer(s_text_layer));
   layer_add_child(window_layer, text_layer_get_layer(s_text_persianDate));
@@ -128,19 +154,7 @@ static void prv_window_unload(Window *window) {
   text_layer_destroy(s_text_todayStepNumber);
 }
 static void update_watchface(){
-  //update time
-  time_t temp = time(NULL);
-  struct tm *tick_time = localtime(&temp);
-  static char s_buffer[8];
-  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
-                                          "%H:%M" : "%I:%M", tick_time);
-  text_layer_set_text(s_text_layer, s_buffer);
-  //update datetime
-  int y, m, d;
-  gregorian_to_jalali(&y, &m, &d,1900+tick_time->tm_year,1+tick_time->tm_mon,tick_time->tm_mday);
-  static char str[20];
-  snprintf(str,sizeof(str), "%d/%d/%d",y,m,d);
-  text_layer_set_text(s_text_persianDate, str);
+  setTodayTimeAndDate(s_text_layer,s_text_persianDate);
 }
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   update_watchface();
@@ -170,7 +184,7 @@ static void prv_init(void) {
      APP_LOG(APP_LOG_LEVEL_INFO, "started watchface %s", "Asking Confirmation");
      break;
  }
- 
+
   s_window = window_create();
   window_set_click_config_provider(s_window, prv_click_config_provider);
   window_set_window_handlers(s_window, (WindowHandlers) {
@@ -187,6 +201,8 @@ static void prv_deinit(void) {
   number_window_destroy(s_window_goals);
   app_worker_message_unsubscribe();
 }
+
+
 
 int main(void) {
   prv_init();
